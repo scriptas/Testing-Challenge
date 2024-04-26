040c30d4c89751fe41c63887425e963956d04525
scriptas <slusnys.antanas@gmail.com>
2024-04-26 17:13:09 +0300

    Convert Cart class to hold 'cart' page info functionality instead of selecting and holding picked items

--- custom-modules/cart.ts
+++ custom-modules/cart.ts
@@ -3,77 +3,77 @@ import { Item } from "./item";
 
 export class Cart {
     private page: Page;
-    items: Item[] = [];
+    private homeUrl: string = "https://shop.polymer-project.org";
 
     constructor(page: Page) {
         this.page = page
     }
 
-    async addItem(itemUrl: string, size: string, quantity: number): Promise<void> {
-        await this.page.goto(itemUrl);
-        await this.selectSize(size);
-        await this.selectQuantity(quantity);
-        await this.clickAddToCartButton();
+    async getShopCartItems(): Promise<Item[]> {
+        await this.goToCart()
+        
+        let shopCartItems: Item[] = []; 
+        const shopCartItemsLocator = this.page.locator("div.list shop-cart-item");
+        const shopCartItemsElements = await shopCartItemsLocator.all();
+        for (const item of shopCartItemsElements) {
+            item.waitFor()
 
-        const singleItemPrice = await this.getItemPrice();
-        const totalPrice = this.calculateTotalPrice(singleItemPrice, quantity);
-        const itemName = await this.getItemName();
-        this.items.push(new Item(itemName, size, quantity, totalPrice));
-    }
+            const nameLocator = item.locator(".name");
+            let itemName = await nameLocator.textContent();
+            if (itemName) {
+                itemName = itemName.trim();
+            } else {
+                throw new Error("Failed to get item name.")
+            }
 
-    private async selectSize(size: string): Promise<void> {
-        const validSizes = ['XS', 'S', 'M', 'L', 'XL'];
-        if (!validSizes.includes(size)) {
-            throw new Error(`Invalid size: ${size}. Valid sizes are: ${validSizes.join(', ')}`);
+            const sizeLocator = item.locator(".size span");
+            let itemSize = await sizeLocator.textContent();
+            if (itemSize) {
+                itemSize = itemSize.trim();
+            } else {
+                throw new Error("Failed to get item size.")
+            }
+
+            const quantitySelectLocator = item.locator("#quantitySelect");
+            const itemQuantity = Number(await quantitySelectLocator.inputValue());
+
+            let itemPrice: number = 0;
+            const priceLocator = item.locator(".price");
+            const pricePerOneItemText = await priceLocator.textContent();
+            if (pricePerOneItemText) {
+                const priceWithoutDollarSign = pricePerOneItemText.replace("$", "");
+                const priceNumber = parseFloat(priceWithoutDollarSign);
+                itemPrice = parseFloat((priceNumber * itemQuantity).toFixed(2));
+            }
+
+            shopCartItems.push(new Item(itemName, itemSize, itemQuantity, itemPrice));
         }
-        const sizeSelect = this.page.locator("#sizeSelect");
-        await sizeSelect.waitFor();
-        await sizeSelect.selectOption(size);
+
+        return shopCartItems;
     }
 
-    private async selectQuantity(quantity: number): Promise<void> {
-        if (quantity < 1 || quantity > 5) {
-            throw new Error(`Invalid quantity: ${quantity}. Must be 1 to 5.`);
-        }
-        const quantitySelect = this.page.locator("#quantitySelect");
-        await quantitySelect.waitFor();
-        await quantitySelect.selectOption(quantity.toString());
-    }
+    async getSubTotal(): Promise<number> {
+        await this.goToCart();
 
-    private async getItemPrice(): Promise<number> {
-        const price = this.page.locator(".price");
-        await price.waitFor();
-        const priceText = await price.textContent();
-        if (priceText) {
-            const priceWithoutDollarSign = priceText.replace('$', '');
-            const priceNumber = parseFloat(priceWithoutDollarSign);
-            return priceNumber;
+        const subTotalLocator = this.page.locator(".subtotal");
+        await subTotalLocator.waitFor();
+
+        const subTotalText = await subTotalLocator.textContent();
+        if (subTotalText) {
+            const subTotalTextNoDollarSign = subTotalText.replace("$", "");
+            return Number(parseFloat(subTotalTextNoDollarSign).toFixed(2));
         } else {
-            throw new Error("Failed to get item price.");
-        }
-    }
-
-    private calculateTotalPrice(singleItemPrice: number, quantity: number): number {
-        return parseFloat((singleItemPrice * quantity).toFixed(2));
-    }
-
-    private async getItemName(): Promise<string> {
-        const name = this.page.locator(".detail h1");
-        await name.waitFor();
-        const nameText = await name.textContent();
-        if (nameText) {
-                return nameText;
-        } else{
-                throw new Error("Failed to get item name.");
+            throw new Error("Failed to get subtotal.")
         }
     }
     
-    private async clickAddToCartButton(): Promise<void> {
-        const addToCartButton = this.page.locator("text=Add to Cart");
-        await addToCartButton.waitFor();
-        await addToCartButton.click();
-
-        const addedToCartNotification = this.page.locator(".label:has-text('Added to cart')");
-        await addedToCartNotification.waitFor();
+    private async goToCart(): Promise<void> {
+        await this.page.goto(this.homeUrl)
+        const cartBtnContainer = this.page.locator(".cart-btn-container a")
+        await cartBtnContainer.waitFor()
+        const href = await cartBtnContainer.getAttribute("href")
+        await this.page.goto(`${this.homeUrl}${href}`)
+        await this.page.waitForLoadState("networkidle")
     }
+
 }
