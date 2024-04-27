abb452eb8b4e59800f453f5ae8e890697dbff45f
scriptas <slusnys.antanas@gmail.com>
2024-04-27 16:56:05 +0300

    Add mobile viewports and branded browsers to 'projects'

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  reportSlowTests: null,
   use: {
     headless: false,
     trace: 'on-first-retry',
@@ -25,5 +26,32 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+
+    // /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+
+    /* Test against branded browsers. */
+    {
+      name: 'Microsoft Edge',
+      use: {
+        ...devices['Desktop Edge'],
+        channel: 'msedge'
+      },
+    },
+    {
+      name: 'Google Chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome'
+      },
+    },
+  
   ],
 });
